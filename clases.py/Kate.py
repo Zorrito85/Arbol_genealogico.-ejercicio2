8766017b73923e7fbdfacf291b8b94506c2428ad
Zorrito85 <manuelantonio.carrascofernand1@usp.ceu.es>
2025-10-30 18:51:19 +0100

    Cambios hechos

---
+++ clases.py/Kate.py
@@ -0,0 +1,8 @@
+from clases.personas import personas
+
+class Kate(personas):
+    def __init__(self, nombre, apellido, apellidoNacimiento, sexo):
+        super().__init__(nombre, apellido, apellidoNacimiento, sexo)
+        
+
+    
